d325102db1287ad343ef776565fdff69b05a4f2e
Joshau2018 <jfklari772@gmail.com>
2024-03-16 21:46:17 -0500

    pl;

---
+++ lab-3.sql
@@ -0,0 +1,35 @@
+-- Joshua Klarich's Lab 3
+USE WideWorldImporters;
+GO
+
+-- Scenerio 1
+UPDATE Application.Cities
+SET LatestRecordedPopulation += 1000
+WHERE CityName in ('Hurst', 'Bedford', 'Euless') 
+      AND StateProvinceID = 45
+   
+-- Scenerio 2
+UPDATE Purchasing.PurchaseOrders
+SET OrderDate = DATEADD(YEAR, 7, OrderDate)
+WHERE OrderDate >= '01/01/2015'
+
+-- Scenerio 3
+SELECT TOP 100 InvoiceID, CustomerID, ( 'XXX-' + RIGHT(CustomerPurchaseOrderNumber, 4)) AS PurchaseOrderNumberMask
+FROM Sales.Invoices
+WHERE  CustomerPurchaseOrderNumber IS NOT NULL
+ORDER BY InvoiceDate DESC, PurchaseOrderNumberMask
+
+-- Scenerio 4
+SELECT TOP 500 CustomerID, CustomerName, ISNULL(CreditLimit, 1000000)
+FROM Sales.Customers
+ORDER BY CreditLimit, CustomerName
+
+-- Scenerio 5
+SELECT CustomerID, ('Tailspin Toys') AS name, Replace(SUBSTRING( CustomerName, 16, LEN(CustomerName)), ')', '') AS location
+FROM Sales.Customers
+WHERE CustomerName LIKE 'Tailspin Toys%'
+ORDER BY location
+
+-- Scenerio 6
+SELECT *
+FROM Application.People
